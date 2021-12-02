7512608ac1dd5e62c3b6efd2c2ec035c9f2dae3a
nazeelaa <92171051+nazeelaa@users.noreply.github.com>
2021-12-02 19:30:29 +0530

    gamble game is pushed

---
+++ gamble.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GamblingGame
+{
+    class gamble
+    {
+        public static void gambleGame()
+        {
+
+
+            double cash, betAmount;
+            Console.WriteLine("Welcome to Gambler Game");
+            Console.WriteLine("Please Enter Cash Amount");
+            cash = double.Parse(Console.ReadLine());
+            Console.WriteLine("Please Enter Bet Amount");
+            betAmount = double.Parse(Console.ReadLine());
+            WinorLoose.winLoose();
+        }
+    }
+}

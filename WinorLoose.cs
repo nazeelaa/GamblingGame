cf701b3eb08f6dfe6dbd062637f219fe81c1cc80
nazeelaa <92171051+nazeelaa@users.noreply.github.com>
2021-12-02 19:32:19 +0530

    UC2 Win or Loose code is pushed

---
+++ WinorLoose.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GamblingGame
+{
+    class WinorLoose
+    {
+        
+        public static void winLoose()
+        {
+            double betamount=1;
+            Console.WriteLine("Betingt amount is $1");
+            if (betamount == 1)
+            {
+                Console.WriteLine("win");
+            }
+
+            else
+            {
+                Console.WriteLine("loose");
+            }
+
+        }
+    }
+}
